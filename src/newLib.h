04ca7bf90bbf13c45684e5126b8518dcee130fdb
Elias Santistevan <elias.santistevan@sparkfun.com>
2021-12-15 13:26:21 -0700

    Renames i2c_comms files to TMF_COMMS
    * cleans out newlib files
    * adds register list to newlib header file

--- src/newLib.h
+++ src/newLib.h
@@ -1,61 +1,95 @@
-#ifndef _HEADER_PROTECTION_NAME_
-#define _HEADER_PROTECTION_NAME_
+#ifndef _SPARKFUN_TMF882X_H_
+#define _SPARKFUN_TMF882X_H_
 
-#include <Wire.h>
-#include <SPI.h>
-#include <Arduino.h>
+#include "TMF_COMMS.h"
 
-#define 
-#define 
-#define 
-
-enum {
+enum TMF882X_REGISTERS {
+  TMF882X_APPID_MAJOR = 0x00, 
+  TMF882X_APPID_MINOR, 
+  TMF882X_PATCH, 
+  TMF882X_BUILD_TYPE, 
+  TMF882X_APP_STAT, 
+  TMF882X_MEAS_STAT, 
+  TMF882X_ALGO_STAT, 
+  TMF882X_CALIB_STAT
+  TMF882X_CMD_STAT
+  TMF882X_PREV_CMD
+  TMF882X_MODE, //TMF8828 ONLY
+  TMF882X_LIVE_BEAT,
+  TMF882X_LIVE_GPIO,
+  TMF882X_SER_NUM_ZERO,
+  TMF882X_SER_NUM_ONE,
+  TMF882X_SER_NUM_TWO,
+  TMF882X_SER_NUM_THREE,
+  TMF882X_CONFIG_RESULT,
+  TMF882X_TID,
+  TMF882X_SIZE_LSB,
+  TMF882X_SIZE_MSB,
+  TMF882X_RES_NUM,
+  TMF882X_TEMP,
+  TMF882X_NUM_VAL_RES,
+  TMF882X_AMB_ZERO,
+  TMF882X_AMB_ONE,
+  TMF882X_AMB_TWO,
+  TMF882X_AMB_THREE,
+  TMF882X_PHOTON_ZERO,
+  TMF882X_PHOTON_ONE,
+  TMF882X_PHOTON_TWO,
+  TMF882X_PHOTON_THREE,
+  TMF882X_REF_COUNT_ZERO,
+  TMF882X_REF_COUNT_ONE,
+  TMF882X_REF_COUNT_TWO,
+  TMF882X_REF_COUNT_THREE,
+  TMF882X_SYS_TICK_ZERO,
+  TMF882X_SYS_TICK_ONE,
+  TMF882X_SYS_TICK_TWO,
+  TMF882X_SYS_TICK_THREE,
+  TMF882X_RES_CONF_ZERO,
+  TMF882X_RES_DIST_ZERO_LSB,
+  TMF882X_RES_DIST_ZERO_MSB,
+  TMF882X_RES_CONF_ONE,
+  TMF882X_RES_DIST_ONE_LSB,
+  TMF882X_RES_DIST_ONE_MSB,
+  //................0xA3
+  TMF882X_PER_MS_LSB = 0x24,
+  TMF882X_PER_MS_MSB,
+  TMF882X_KILO_ITER_LSB,
+  TMF882X_KILO_ITER_MSB,
+  TMF882X_INT_THRESH_LOW_LSB,
+  TMF882X_INT_THRESH_LOW_MSB,
+  TMF882X_INT_THRESH_HIGH_LSB,
+  TMF882X_INT_THRESH_HIGH_MSB,
+  TMF882X_INT_ZONE_MASK_ZERO,
+  TMF882X_INT_ZONE_MASK_ONE,
+  TMF882X_INT_ZONE_MASK_TWO,
+  TMF882X_INT_PERSIST,
+  TMF882X_CONF_THRESH,
+  TMF882X_GPIO_ZERO,
+  TMF882X_GPIO_ONE,
+  TMF882X_POWER_CFG,
+  TMF882X_SPAD_MAP_ID,
+  TMF882X_ALG_SET_ZERO,
+  //....RESERVERD 0x36 - 0x38
+  TMF882X_HIST_DUMP = 0x39,
+  TMF882X_I2C_ADDR,
+  TMF882X_OSC_TRIM_LSB,
+  TMF882X_OSC_TRIM_MSB,
+  TMF882X_I2C_CHANGE,
+  //......SPAD REGS SKIPPED
+  //.....CROSSTALK REGS SKIPPED  
 };
 
-class Class_Name
+class QwiicTMF882X
 {  
   public:
     
-    // Public Variables
-    
-    //Function declarations
-    Class_Name(); // SPI Constructor
-    Class_Name(uint8_t address); // I2C Constructor
-
-    bool begin( TwoWire &wirePort );
-    bool beginSpi(uint8_t userCsPin, SPIClass &spiPort);
+    QwiicTMF882X(); // SPI Constructor
+    QwiicTMF882X(uint8_t address); // I2C Constructor
 
     bool begin(TwoWire &wirePort = Wire); // begin function
-    bool beginSpi(SPIClass &spiPort = SPI); 
+    bool beginSpi(uint8_t userCsPin, SPIClass &spiPort = SPI);
 
   private:
     
-    // Private Variables
-    uint8_t _address;
-
-    //SPI settings class sets the speed, chip select, and bit order
-    SPISettings mySpi; 
-
-    // This generic function handles I2C write commands for modifying individual
-    // bits in an eight bit register. Paramaters include the register's address, a mask 
-    // for bits that are ignored, the bits to write, and the bits' starting
-    // position.
-    void writeRegister(uint8_t _wReg, uint8_t _mask, uint8_t _bits, uint8_t _startPosition);
-
-    // This generic function does a basic I-squared-C write transaction at the
-    // given address, and writes the given _command argument. 
-    void _writeCommand(uint8_t _command);
-
-    // This generic function reads an eight bit register. It takes the register's
-    // address as its' parameter. 
-    uint8_t readRegister(uint8_t _reg);
-
-    // This generic function does a basic I-squared-C read transaction at the given
-    // addres, taking the number of reads as argument. 
-    uint8_t _readCommand(uint8_t _numReads);
-
-
-    TwoWire *_i2cPort;
-    SPIClass *_spiPort;
 };
 #endif
