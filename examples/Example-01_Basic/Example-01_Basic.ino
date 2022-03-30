dfa62078a4919bb5495ec4abb486ca967b49e3da
Kirk <blowme@cow.com>
2022-03-30 13:55:22 -0600

    cleanup

--- examples/Example-01_Basic/Example-01_Basic.ino
+++ examples/Example-01_Basic/Example-01_Basic.ino
@@ -18,13 +18,13 @@ void setup(){
 	Serial.println("In setup");
 	Serial.println("==============================");
 
-
 	if(!myTMF882X.begin())
 	{
 		Serial.println("Error - The TMF882X failed to initialize - is the board connected?");
 		while(1);
 	}else
 		Serial.println("TMF882X started.");
+
 
 }
 
@@ -37,17 +37,17 @@ void loop()
     {
     	// print out results
     	Serial.println("Measurement:");
-    	Serial.print("Result Number: "); Serial.print(myResults.result_num);
-    	Serial.print(" Number of Results: "); Serial.println(myResults.num_results);    	
+    	Serial.print("     Result Number: "); Serial.print(myResults.result_num);
+    	Serial.print("  Number of Results: "); Serial.println(myResults.num_results);    	
 
 	    for (uint32_t i = 0; i < myResults.num_results; ++i) {
-	    	Serial.print("    conf: "); Serial.print(myResults.results[i].confidence);
+	    	Serial.print("       conf: "); Serial.print(myResults.results[i].confidence);
 	    	Serial.print(" distance mm: "); Serial.print(myResults.results[i].distance_mm);
 	    	Serial.print(" channel: "); Serial.print(myResults.results[i].channel);
 	    	Serial.print(" sub_capture: "); Serial.println(myResults.results[i].sub_capture);	
 
 	    }
-	   	Serial.print(" photon: "); Serial.print(myResults.photon_count);	
+	   	Serial.print("     photon: "); Serial.print(myResults.photon_count);	
 	   	Serial.print(" ref photon: "); Serial.print(myResults.ref_photon_count);
 	   	Serial.print(" ALS: "); Serial.println(myResults.ambient_light); Serial.println();
 
